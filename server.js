fc344be955c2198441137ae0188f7454a37ea38a
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2022-09-06 12:40:56 +0200

    Added DOM manip and client-server communication

--- server.js
+++ server.js
@@ -7,6 +7,4 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/ws.html');
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(3010);
--- ws.js
+++ ws.js
@@ -1,13 +1,15 @@
 var WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({port: 40510})
 
+let counter = 0;
+
 wss.on('connection', function (ws) {
   ws.on('message', function (message) {
     console.log('received: %s', message)
   })
 
   setInterval(
-    () => ws.send(`${new Date()}`),
-    1000
+    () => ws.send(`Hello from Orwa (#${counter++})`),
+    5000
   )
 })
